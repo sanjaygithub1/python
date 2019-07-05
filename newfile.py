0fc6392c09e9594cd5dfc223963d2da1ae7a8e23
sanjaygithub1 <sanjay.gupta@infostretch.com>
2019-07-05 15:20:28 +0530

    just a first pgm

--- newfile.py
+++ newfile.py
@@ -1,7 +1,7 @@
-print("hello world")
-items=["items1","items2","items3"]
-print(items[0])
-base=10
-height=5
-area = 1/2 *(base*height)
-print("area of a triangle is:", area)
+num= input("Enter a number: ")
+num=int(num)
+if num%2==0:
+    print ("the given number is Even")
+else:
+    print("the given number is Odd")
+
