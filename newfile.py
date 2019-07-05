025352a9047917de7d6a176d7748867eee7d3db8
sanjaygithub1 <sanjay.gupta@infostretch.com>
2019-07-05 15:29:07 +0530

    just a first pgm

--- newfile.py
+++ newfile.py
@@ -4,4 +4,13 @@ if num%2==0:
     print ("the given number is Even")
 else:
     print("the given number is Odd")
+indian=["samosa", "daal"]
+chinese=["egg role", "manchurian"]
 
+dish=input("Enter dish name: ")
+if dish in indian:
+    print("dish is indian")
+elif dish in chinese:
+    print("dish is chinese")
+else:
+    print("unknown dish")
