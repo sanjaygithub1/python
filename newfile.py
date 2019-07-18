024f7b1de8dc15ba580df9611ec458cedd31e99f
sanjaygithub1 <sanjay.gupta@infostretch.com>
2019-07-18 17:55:59 +0530

    just a first pgm

--- newfile.py
+++ newfile.py
@@ -1,27 +1,12 @@
-num= input("Enter a number: ")
-num=int(num)
-if num%2==0:
-    print ("the given number is Even")
-else:
-    print("the given number is Odd")
-indian=["samosa", "daal"]
-chinese=["egg role", "manchurian"]
-
-dish=input("Enter dish name: ")
-if dish in indian:
-    print("dish is indian")
-elif dish in chinese:
-    print("dish is chinese")
-else:
-    print("unknown dish")
-
-exp=[1000, 2000, 3000]
-total=0
-for item in exp:
-    total = total + item
-
-print(total)
+class Accident(Exception):
+    def __init__(self,msg):
+        self.msg=msg
+    def print_exception(self):
+        print("User defined exception:", self.msg)
 
 
-for i in range(1,12):
- print(i)
+try:
+    raise Accident ('crash between two cars')
+except Accident as p:
+    p.print_exception()
+
